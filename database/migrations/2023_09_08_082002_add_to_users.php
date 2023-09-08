d4e73028d769a48beabc288913c0371c37e536d9
shvalin1 <shvalin01@gmail.com>
2023-09-08 17:33:12 +0900

    usersテーブルにカラムを追加 #15

---
+++ database/migrations/2023_09_08_082002_add_to_users.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('icon'); //pathを保存
+            $table->string('account_name');
+            $table->string('url');
+            $table->bigInteger('point');
+            //
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['icon', 'account_name', 'url', 'point']);
+        });
+    }
+};
