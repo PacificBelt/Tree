2a089e176b3db9a3de9059509f636b422bf83dfc
shvalin1 <shvalin01@gmail.com>
2023-09-08 18:37:49 +0900

    usersのseederを修正 #15

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,9 +23,12 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'icon' => 'default_icon.png',
+            'account_name' => $this->faker->userName(), // ランダムなアカウント名
+            'url' => $this->faker->url(), // ランダムなURL
+            'point' => $this->faker->numberBetween(10000, 1000000), // ランダムなポイント
         ];
     }
-
     /**
      * Indicate that the model's email address should be unverified.
      */
