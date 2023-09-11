d9ed79ffc1915766dcd1987a12ae9dda08ae5470
shvalin1 <shvalin01@gmail.com>
2023-09-11 19:15:04 +0900

    プロジェクトごとの合計寄附金額を取得する処理をモデルに追加 #1

--- app/Models/payments.php
+++ app/Models/payments.php
@@ -8,4 +8,28 @@ use Illuminate\Database\Eloquent\Model;
 class payments extends Model
 {
     use HasFactory;
+    public static function getCurrentAmount()
+    {
+        $data = self::orderBy('updated_at', 'desc')->get();
+        #project_idごとに合計金額と寄付人数をを計算する
+        $currentAmount = [];
+        $numDonations = [];
+
+        foreach ($data as $key => $value) {
+            // 合計金額を計算
+            if (array_key_exists($value->project_id, $currentAmount)) {
+                $currentAmount[$value->project_id] += $value->amount;
+            } else {
+                $currentAmount[$value->project_id] = $value->amount;
+            }
+
+            // 寄付人数を計算
+            if (array_key_exists($value->project_id, $numDonations)) {
+                $numDonations[$value->project_id] += 1;
+            } else {
+                $numDonations[$value->project_id] = 1;
+            }
+        }
+        return [$currentAmount, $numDonations];
+    }
 }
