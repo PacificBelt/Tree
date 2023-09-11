aa67311653f9b3c8b5978f6d7aa4f5c3bfde7549
naka-12 <104970808+naka-12@users.noreply.github.com>
2023-09-11 16:58:59 +0900

    マイページメニュー追加 #17

--- resources/js/Components/Header.jsx
+++ resources/js/Components/Header.jsx
@@ -4,32 +4,22 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
 import AccountCircle from "@mui/icons-material/AccountCircle";
-import MenuItem from "@mui/material/MenuItem";
-import Menu from "@mui/material/Menu";
+import MyPageMenu from "./MyPageMenu";
 
 export default function MenuAppBar() {
     const [auth, setAuth] = useState(true);
-    const [anchorEl, setAnchorEl] = useState(null);
+    const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
-    const handleChange = (event) => {
-        setAuth(event.target.checked);
-    };
-
-    const handleMenu = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
-
-    const handleClose = () => {
-        setAnchorEl(null);
+    const toggleDrawer = () => {
+        setIsDrawerOpen(!isDrawerOpen);
     };
 
     return (
-        <AppBar position="sticky" sx={{mb: 2}}>
+        <AppBar position="sticky" sx={{ mb: 2 }}>
             <Toolbar>
                 <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                     Tree
                 </Typography>
-                {/* TODO: マイページ実装 */}
                 {auth && (
                     <div>
                         <IconButton
@@ -37,31 +27,12 @@ export default function MenuAppBar() {
                             aria-label="account of current user"
                             aria-controls="menu-appbar"
                             aria-haspopup="true"
-                            onClick={handleMenu}
+                            onClick={toggleDrawer}
                             color="inherit"
                         >
                             <AccountCircle />
                         </IconButton>
-                        <Menu
-                            id="menu-appbar"
-                            anchorEl={anchorEl}
-                            anchorOrigin={{
-                                vertical: "top",
-                                horizontal: "right",
-                            }}
-                            keepMounted
-                            transformOrigin={{
-                                vertical: "top",
-                                horizontal: "right",
-                            }}
-                            open={Boolean(anchorEl)}
-                            onClose={handleClose}
-                        >
-                            <MenuItem onClick={handleClose}>Profile</MenuItem>
-                            <MenuItem onClick={handleClose}>
-                                My account
-                            </MenuItem>
-                        </Menu>
+                        <MyPageMenu isOpen={isDrawerOpen} toggleDrawer={toggleDrawer}/>
                     </div>
                 )}
             </Toolbar>
---
+++ resources/js/Components/MyPageMenu.jsx
@@ -0,0 +1,91 @@
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import EditNoteIcon from '@mui/icons-material/EditNote';
+import LogoutIcon from "@mui/icons-material/Logout";
+import ManageAccountsIcon from "@mui/icons-material/ManageAccounts";
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import CloseIcon from "@mui/icons-material/Close";
+import styled from "@emotion/styled";
+import { IconButton, Typography } from "@mui/material";
+
+export default function MyPageMenu(props) {
+    const { isOpen, toggleDrawer } = props;
+
+    const DrawerHeader = styled("div")(() => ({
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        padding: "0 8px",
+        height: "64px",
+    }));
+
+    const list = () => (
+        <Box sx={{ width: 300 }} role="presentation" onClick={toggleDrawer}>
+            <List>
+                <ListItem disablePadding>
+                    {/* TODO: リンク先を設定する */}
+                    <ListItemButton href="">
+                        <ListItemIcon>
+                            <EditNoteIcon />
+                        </ListItemIcon>
+                        <ListItemText primary={"作成したプロジェクト"} />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    {/* TODO: リンク先を設定する */}
+                    <ListItemButton href="">
+                        <ListItemIcon>
+                            <FavoriteIcon />
+                        </ListItemIcon>
+                        <ListItemText primary={"支援したプロジェクト"} />
+                    </ListItemButton>
+                </ListItem>
+            </List>
+            <Divider />
+            <List>
+                <ListItem disablePadding>
+                  {/* TODO: リンク先を設定する */}
+                    <ListItemButton>
+                        <ListItemIcon>
+                            <ManageAccountsIcon />
+                        </ListItemIcon>
+                        <ListItemText primary={"会員情報編集"} />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                  {/* TODO: リンク先を設定する */}
+                    <ListItemButton>
+                        <ListItemIcon>
+                            <LogoutIcon />
+                        </ListItemIcon>
+                        <ListItemText primary={"ログアウト"} />
+                    </ListItemButton>
+                </ListItem>
+            </List>
+        </Box>
+    );
+
+    return (
+        <div>
+            <>
+                <Drawer anchor="right" open={isOpen} onClose={toggleDrawer}>
+                    <DrawerHeader>
+                        <Typography sx={{ pl: 2 }} variant="h6">
+                            メニュー
+                        </Typography>
+                        <IconButton onClick={toggleDrawer}>
+                            <CloseIcon />
+                        </IconButton>
+                    </DrawerHeader>
+                    {list()}
+                </Drawer>
+            </>
+        </div>
+    );
+}
