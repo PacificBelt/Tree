66ccee18d1220eb8f88065b1d7217a5f93746ed4
naka-12 <104970808+naka-12@users.noreply.github.com>
2023-09-11 13:49:25 +0900

    一覧のカード表示を追加

--- resources/js/Pages/Projects.jsx
+++ resources/js/Pages/Projects.jsx
@@ -1,9 +1,20 @@
+import Cards from "@/Components/Cards";
 import Header from "@/Components/Header";
 
 export default function Projects() {
+    const items = Array(50).fill({
+        title: "ほげほげ",
+        content:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos aliquam ducimus consectetur mollitia atque in consequuntur soluta hic, deserunt ut dolor culpa. Voluptates, a dolore aut esse ea dicta ab!",
+        createdBy: "ほげ 太郎",
+        currentAmount: 10000,
+        goalAmount: 100000,
+        numDonations: 54,
+    });
     return (
         <>
             <Header />
+            <Cards items={items} />
         </>
     );
 }
