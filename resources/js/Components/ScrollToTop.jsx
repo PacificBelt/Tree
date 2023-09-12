d652a2fc46f3e43ed62893431ff42ea337d5ea9a
S.Morimoto <72482515+depe874@users.noreply.github.com>
2023-09-12 14:55:07 +0900

    tailwindで試作

---
+++ resources/js/Components/ScrollToTop.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from "react";
+import { BiChevronUp } from "react-icons/bi";
+
+const ScrollToTop = () => {
+    const [display, setDisplay] = useState(false);
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            setDisplay(window.scrollY > 400);
+        });
+    }, []);
+    return (
+        <>
+            {display && (
+                <div className="fixed right-[5%] bottom-40 w-8 h-8 bg-pink flex items-center justify-center rounded-full">
+                    <a href="#home">
+                        <BiChevronUp className="text-3xl text-white" />
+                    </a>
+                </div>
+            )}
+        </>
+    );
+};
+
+export default ScrollToTop;
