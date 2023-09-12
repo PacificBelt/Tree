718e3b2fdb27c94e7840a9589a435b667261b4d2
shvalin1 <shvalin01@gmail.com>
2023-09-12 21:50:04 +0900

    ログアウトできるように実装 #87

--- resources/js/Components/MyPageMenu.jsx
+++ resources/js/Components/MyPageMenu.jsx
@@ -6,13 +6,14 @@ import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import EditNoteIcon from '@mui/icons-material/EditNote';
+import EditNoteIcon from "@mui/icons-material/EditNote";
 import LogoutIcon from "@mui/icons-material/Logout";
 import ManageAccountsIcon from "@mui/icons-material/ManageAccounts";
-import FavoriteIcon from '@mui/icons-material/Favorite';
+import FavoriteIcon from "@mui/icons-material/Favorite";
 import CloseIcon from "@mui/icons-material/Close";
 import styled from "@emotion/styled";
 import { IconButton, Typography } from "@mui/material";
+import { Inertia } from "@inertiajs/inertia";
 
 export default function MyPageMenu(props) {
     const { isOpen, toggleDrawer } = props;
@@ -50,16 +51,11 @@ export default function MyPageMenu(props) {
             <Divider />
             <List>
                 <ListItem disablePadding>
-                    <ListItemButton href="profile">
-                        <ListItemIcon>
-                            <ManageAccountsIcon />
-                        </ListItemIcon>
-                        <ListItemText primary={"会員情報編集"} />
-                    </ListItemButton>
-                </ListItem>
-                <ListItem disablePadding>
-                  {/* TODO: リンク先を設定する */}
-                    <ListItemButton>
+                    <ListItemButton
+                        onClick={() => {
+                            Inertia.post(route("logout"));
+                        }}
+                    >
                         <ListItemIcon>
                             <LogoutIcon />
                         </ListItemIcon>
