bbf67011930ae8ebb23bf439dc385b6f7217a4cf
shvalin1 <shvalin01@gmail.com>
2023-09-12 14:04:46 +0900

    propsを受け取って表示するように変更 #46

--- resources/js/Pages/ProjectDetail.jsx
+++ resources/js/Pages/ProjectDetail.jsx
@@ -1,32 +1,44 @@
-import { Box, Button, Link, Paper, TextField, Typography, Grid, Card } from "@mui/material";
+import {
+    Box,
+    Button,
+    Link,
+    Paper,
+    TextField,
+    Typography,
+    Grid,
+    Card,
+} from "@mui/material";
 import { Stack, Chip } from "@mui/material";
-import SellIcon from '@mui/icons-material/Sell';
-import PersonIcon from '@mui/icons-material/Person';
-import GroupsIcon from '@mui/icons-material/Groups';
-import AlarmIcon from '@mui/icons-material/Alarm';
+import SellIcon from "@mui/icons-material/Sell";
+import PersonIcon from "@mui/icons-material/Person";
+import GroupsIcon from "@mui/icons-material/Groups";
+import AlarmIcon from "@mui/icons-material/Alarm";
 
-import LinearProgress from '@mui/material/LinearProgress';
+import LinearProgress from "@mui/material/LinearProgress";
+import { useEffect } from "react";
 
-export default function ProjectDetail() {
+export default function ProjectDetail(props) {
+    const { item } = props.project;
+    useEffect(() => {
+        const { item } = props.project;
+        console.log(props.project);
+    }, []);
+
     return (
         <Paper
             sx={{
                 p: 4,
                 width: "95%",
                 m: "30px auto",
-                background:"#e8f5e9"
+                background: "#e8f5e9",
             }}
         >
-
-            <Typography
-                variant={"h3"}
-                align="center"
-                mb={5}
-            >
-                [プロジェクト名]
-            </Typography>
+            <Typography variant={"h3"} align="center" mb={5}></Typography>
             <Box align="center" m={2}>
-                <Chip icon={<PersonIcon fontSize="small" />} label={"[プロジェクト作者名]"} />
+                <Chip
+                    icon={<PersonIcon fontSize="small" />}
+                    label={props.project.userName}
+                />
                 {/* <Chip icon={<SellIcon fontSize="small" />} label={"[ハッシュタグ名]"} /> */}
             </Box>
             <Grid
@@ -42,12 +54,14 @@ export default function ProjectDetail() {
                             <img src="https://images.unsplash.com/photo-1551963831-b3b1ca40c98e"></img>
                         </Box>
                         <Box m={3}>
-                            <Typography variant={"subtitle1"}>
-                                [プロジェクトの概要]
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit,
-                                quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum
-                                fugiat deleniti? Eum quasi quidem quibusdam.
-                            </Typography>
+                            <div
+                                dangerouslySetInnerHTML={{
+                                    __html: props.project.description.replace(
+                                        /\n/g,
+                                        "<br />"
+                                    ),
+                                }}
+                            ></div>
                         </Box>
                     </Card>
                 </Grid>
@@ -58,9 +72,12 @@ export default function ProjectDetail() {
                                 ¥ 現在の支援金総額
                             </Typography>
                             <Typography variant={"h3"}>
-                                123,456,789円
+                                {props.project.currentAmount.toLocaleString()}円
                             </Typography>
-                            <linearGradient id="linearColors" gradientTransform="rotate(90)">
+                            <linearGradient
+                                id="linearColors"
+                                gradientTransform="rotate(90)"
+                            >
                                 <stop offset={0} stopColor="#0080FE" />
                                 <stop offset={1} stopColor="#00DDDD" />
                             </linearGradient>
@@ -70,41 +87,44 @@ export default function ProjectDetail() {
                                 sx={{
                                     height: "10px",
                                     borderRadius: "5px",
-                                    background: 'lightgrey',
+                                    background: "lightgrey",
                                 }}
                             />
                             <Typography variant="subtitle1">
                                 達成率 60%
                             </Typography>
                             <Typography variant="subtitle1">
-                                目標金額 {(123456789 / 0.6).toLocaleString()}円
+                                目標金額{" "}
+                                {props.project.goal_amount.toLocaleString()}円
                             </Typography>
                         </Box>
-                        <Box m={3} >
+                        <Box m={3}>
                             <Stack direction="row">
-                                <GroupsIcon fontSize="medium" sx={{ margin: "5px" }} />
-                                <Typography variant="h6">
-                                    支援者数
-                                </Typography>
+                                <GroupsIcon
+                                    fontSize="medium"
+                                    sx={{ margin: "5px" }}
+                                />
+                                <Typography variant="h6">支援者数</Typography>
                             </Stack>
                             <Typography variant={"h4"}>
-                                123人
+                                {props.project.numDonations.toLocaleString()}人
                             </Typography>
                         </Box>
-                        <Box m={3} >
+                        <Box m={3}>
                             <Stack direction="row">
-                                <AlarmIcon fontSize="medium" sx={{ margin: "5px" }} />
-                                <Typography variant="h6">
-                                    締め切りまで
-                                </Typography>
+                                <AlarmIcon
+                                    fontSize="medium"
+                                    sx={{ margin: "5px" }}
+                                />
+                                <Typography variant="h6">締め切り</Typography>
                             </Stack>
                             <Typography variant={"h4"}>
-                                50日
+                                {props.project.deadline}
                             </Typography>
                         </Box>
                     </Card>
                 </Grid>
             </Grid>
-        </ Paper>
+        </Paper>
     );
 }
