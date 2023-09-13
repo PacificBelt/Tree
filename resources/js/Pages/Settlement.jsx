df46aa0633618f191161a5c447d8f2efdb815163
karen <k.renren0119@gmail.com>
2023-09-13 11:19:10 +0900

    DB追加できないエラー解消 #11

--- resources/js/Pages/Settlement.jsx
+++ resources/js/Pages/Settlement.jsx
@@ -1,15 +1,18 @@
-import { Box, Button, Link, Paper, TextField, Typography, InputAdornment } from "@mui/material";
+import { Box, Button, Link, Paper, TextField, Typography, InputAdornment , Stack} from "@mui/material";
 import { Radio, RadioGroup, FormControlLabel, FormControl, FormLabel } from "@mui/material";
 import CreditCardTwoToneIcon from '@mui/icons-material/CreditCardTwoTone';
 import QrCode2TwoToneIcon from '@mui/icons-material/QrCode2TwoTone';
-import { useForm } from '@inertiajs/react';
+import { useForm, usePage } from '@inertiajs/react';
+import Header from '@/Components/Header';
+import Footer from '@/Components/Footer';
 
 export default function Settlement(props) {
+    const auth = usePage().props.auth;
     const { project } = props;
 
     const { data, setData, post, processing, errors, reset } = useForm({
         amount: '',
-        
+
     });
 
     const submit = (e) => {
@@ -19,74 +22,74 @@ export default function Settlement(props) {
     };
 
     return (
-        <Paper
-            sx={{
-                p: 4,
-                width: "500px",
-                m: "30px auto",
-            }}
-        >
-            <Typography variant={"h5"}>支援内容</Typography>
-            <form onSubmit={submit}>
-                <Box m={2}>
-                    <Typography variant={"h4"}>{project.title}</Typography>
-                </Box>
-                <Box mt={2}>
-                    <TextField
-                        label="支援金額"
-                        variant="standard"
-                        required
-                        sx={{ mt: 1 }}
-                        type="number"
-                        value={data.amount}
-                        onChange={(e) => setData('amount', e.target.value)}     
-                        InputProps={{
-                            startAdornment: (
-                                <InputAdornment position="start">
-                                    ¥
-                                </InputAdornment>
-                            ),
-                        }}
-                    >
-                    </TextField>
-                </Box>
-                <Box mt={2}>
-
-                    <FormControl>
-                        <FormLabel id="demo-radio-buttons-group-label">支援方法選択</FormLabel>
-                        <RadioGroup
-                            aria-labelledby="demo-radio-buttons-group-label"
-                            defaultValue="credit"
-                            name="radio-buttons-group"
+        <>
+            <Header auth={auth} />
+            <Paper
+                sx={{
+                    p: 4,
+                    width: "500px",
+                    m: "30px auto",
+                }}
+            >
+                <Typography variant={"h5"}>支援内容</Typography>
+                <form onSubmit={submit}>
+                    <Box m={2}>
+                        <Typography variant={"h4"}>{project.title}</Typography>
+                    </Box>
+                    <Box mt={2}>
+                        <TextField
+                            label="支援金額"
+                            variant="standard"
+                            required
+                            sx={{ mt: 1 }}
+                            type="number"
+                            value={data.amount}
+                            onChange={(e) => setData('amount', e.target.value)}
+                            InputProps={{
+                                startAdornment: (
+                                    <InputAdornment position="start">
+                                        ¥
+                                    </InputAdornment>
+                                ),
+                            }}
                         >
-                            <FormControlLabel value="credit" control={
-                                <>
-                                    <Radio size="small" />
-                                    <CreditCardTwoToneIcon />
-                                </>
-                            } label="各種クレジットカード">
+                        </TextField>
+                    </Box>
+                    <Box mt={2}>
 
-                            </FormControlLabel>
-                            <FormControlLabel value="バーコード決済" control={
-                                <>
-                                    <Radio size="small" />
+                        <FormControl>
+                            <FormLabel id="demo-radio-buttons-group-label">支援方法選択</FormLabel>
+                            <RadioGroup
+                                aria-labelledby="demo-radio-buttons-group-label"
+                                defaultValue="credit"
+                                name="radio-buttons-group"
+                            >
+                                <Stack direction="row" alignItems="center">
+                                    <FormControlLabel value="credit" control={<Radio size="small" />} />
+                                    <CreditCardTwoToneIcon />
+                                    <Typography>各種クレジットカード</Typography>
+                                </Stack>
+                                <Stack direction="row" alignItems="center">
+                                    <FormControlLabel value="barcord" control={<Radio size="small" />} />
                                     <QrCode2TwoToneIcon />
-                                </>
-                            } label="バーコード決済" />
-                        </RadioGroup>
-                    </FormControl>
-                </Box>
-                <Button
-                    type="submit"
-                    color="primary"
-                    variant="contained"
-                    fullWidth
-                    sx={{ mt: 4 }}
-                    disabled={processing}
-                >
-                    支援する
-                </Button>
-            </form>
-        </Paper >
+                                    <Typography>バーコード決済</Typography>
+                                </Stack>
+                            </RadioGroup>
+                        </FormControl>
+                    </Box>
+                    <Button
+                        type="submit"
+                        color="primary"
+                        variant="contained"
+                        fullWidth
+                        sx={{ mt: 4 }}
+                        disabled={processing}
+                    >
+                        支援する
+                    </Button>
+                </form>
+            </Paper >
+            <Footer />
+        </>
     );
 }
--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,10 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\SearchController;
 use App\Http\Controllers\PaymentController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-use SebastianBergmann\CodeCoverage\Report\Xml\Project;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,42 +18,43 @@ use SebastianBergmann\CodeCoverage\Report\Xml\Project;
 |
 */
 
-Route::resource('project', ProjectController::class);
+Route::get('/', [ProjectController::class, 'index']);
+Route::post('/project/search', [SearchController::class, 'index'])->name('project.search');
 
-Route::get('/', function () {
-    return Inertia::render('Projects', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+Route::group(
+    ['middleware' => 'auth'],
+    function () {
+        Route::resource('project', ProjectController::class); //いろいろなルーティングが含まれるので超超超注意！！
 
-Route::get('/project/show/{id}', [ProjectController::class, 'show'])->name('project.show');
-Route::get('/dashboard', [ProjectController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
+        Route::get('/mypage/created', [SearchController::class, 'created'])->name('project.created');
+        Route::get('/mypage/donated', [SearchController::class, 'donated'])->name('project.donated');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+        Route::get('/dashboard', [ProjectController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
+
+        Route::middleware('auth')->group(function () {
+            Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+            Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+            Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+        });
+
+        Route::get('/project', function () {
+            return Inertia::render('ProjectDetail');
+        })->middleware(['auth', 'verified'])->name('project');
+
+        Route::get('/project/create', [ProjectController::class, 'create'])
+            ->middleware(['auth', 'verified'])->name('project.create');
+        Route::post('/project/create', [ProjectController::class, 'store']);
+
+        Route::get('/show/{id}', [ProjectController::class, 'show'])->name('show');
+
+        Route::get('/project/edit/{id}', [ProjectController::class, 'edit'])->name('project.edit');
+        Route::post('/project/edit/{id}', [ProjectController::class, 'update']);
+
+        Route::get('/payment/{id}', [PaymentController::class, 'create'])
+            ->middleware(['auth', 'verified'])->name('payment');
+        Route::post('/payment/{id}', [PaymentController::class, 'store']);
+    }
+);
 
 
-Route::get('/project', function () {
-    return Inertia::render('ProjectDetail');
-})->middleware(['auth', 'verified'])->name('project');
-
-Route::get('/project/create', [ProjectController::class, 'create'])
-->middleware(['auth', 'verified'])->name('project.create');
-Route::post('/project/create', [ProjectController::class, 'store']);
-
-Route::get('/project/edit/{id}', [ProjectController::class, 'edit'])->name('project.edit');
-Route::post('/project/edit/{id}', [ProjectController::class, 'update']);
-
-Route::get('/payment/{id}', [PaymentController::class, 'create'])
-->middleware(['auth', 'verified'])->name('payment');
-Route::post('/payment/{id}', [PaymentController::class, 'store']);
-
-
-require __DIR__.'/auth.php';
-
+require __DIR__ . '/auth.php';
